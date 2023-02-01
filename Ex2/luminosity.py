e084fef036a3047e3ffa518e3c0b7e30433a18dd
Alexi Stadnitski <alexi.stadnitski@helsinki.fi>
2023-02-01 21:59:25 +0200

    Uploading third part of exercise

--- Ex2/luminosity.py
+++ Ex2/luminosity.py
@@ -2,25 +2,40 @@ import linecache
 import numpy as np
 import re
 
-#data = np.loadtxt('brilcalc.log', dtype = '', 'delimiter = '|')
+### Exercise 2 : 2 ###
 
-l = 333.333333333
+luminosity = 0
 
 d = re.compile('\|')
 p = re.compile('totrecorded')
 n = re.compile('\d*\.\d*')
 
-cell = [0, 0]
+cell = 0
 
 for row, line in enumerate(open('brilcalc.log')):
-    col = 0
-    for match in re.finditer(p, line):
-        for match in re.finditer(d, line):
-            col += 1
-        cell = [row + 1, col - 1]
+    for match in re.finditer(p, line): cell = row + 1
 
-line = linecache.getline('brilcalc.log', cell[0] + 2)
+line = linecache.getline('brilcalc.log', cell + 2)
 for match in re.finditer(n, line):
-    l = float(match.group())
+    luminosity = float(match.group())
 
-print('Luminosity:', '{:.1f}'.format(l * 1e-3), '/fb')
+print('- Total recorded luminosity:', '{:.1f}'.format(luminosity * 1e-3), '/fb')
+
+### Exercise 2 : 3 ###
+
+runs, compare = [], []
+s = re.compile('Summary')
+total = 0
+
+for row, line in enumerate(open('brilcalc.log')):
+    for match in re.finditer(n, line):
+        compare.append(float(match.group()))
+    try: runs.append(np.min(compare))
+    except:
+        for match in re.finditer(s, line): total = np.sum(runs)
+    compare = []
+
+
+print('- Calculated sum of recorded luminosities:', total, '/pb or', total * 1e-3, '/fb')
+print('- Difference to expected:', np.abs(total - luminosity))
+print('- Results confirmed:', 1e-8 > np.abs(total - luminosity), '\n- Difference in significant figures is small enough that it is likely the result of rounding the 10th digit past the decimal point')
