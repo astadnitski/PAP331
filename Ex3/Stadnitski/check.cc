992e083cf8f1bf290308e72085a3a56d1d3ec892
Alexi Stadnitski <alexi.stadnitski@helsinki.fi>
2023-02-09 13:32:37 +0200

    Updating exercise

--- Ex3/Stadnitski/check.cc
+++ Ex3/Stadnitski/check.cc
@@ -20,8 +20,8 @@ int main(int argc, char** argv) {
   measurement.setRefY(vector <float> {2, 6, 7, 3, 3, 2, 2, 8, 0, 1}); // Ex2 shOut % 9
   matrix error = measurement.getError(x, y);
 
-  ofstream info; //info.open("measurement_" + to_string(x) + "_" + to_string(y) + ".txt");
-  info.open("measurement.txt");
+  ofstream info; string filename = "measurement.txt"; //info.open("measurement_" + to_string(x) + "_" + to_string(y) + ".txt");
+  info.open(filename);
   info << "Measurement: (" << x << ", " << y << ")" << endl;
   info << "Error matrix" << endl;
   info << error.mtx[0][0] << " | " << error.mtx[0][1] << endl;
@@ -29,5 +29,7 @@ int main(int argc, char** argv) {
   info << "Distance: " << measurement.getDist(x, y) << " +/- " << measurement.getDistErr(x, y) << endl;
   info << "Significance: " << measurement.getSig(x, y) << endl;
   info.close();
+
+  cout << "Output saved to " << filename << endl;
 
 }
