5c777876b202c51d55acd1449a3d0941f5f134ee
Alexi Stadnitski <alexi.stadnitski@helsinki.fi>
2023-02-20 20:28:41 +0200

    Uploading exercise

---
+++ Ex5/Stadnitski/normal.cxx
@@ -0,0 +1,20 @@
+void normal() {
+
+    int N = 1000;
+    TRandom* rng = new TRandom;
+    TFile* normals = TFile::Open("normals.root", "RECREATE");
+    TTree* tree = new TTree("tree", "Tree of normally distributed numbers");
+    
+    Float_t n;
+    tree -> Branch("n", &n, "n/F");
+    
+    for (int i = 0; i < N; i++) {
+        n = rng -> Gaus(0, 1);
+        tree -> Fill();
+    }
+    
+    tree -> Write();
+    normals -> Close();
+    exit(0);
+
+}
