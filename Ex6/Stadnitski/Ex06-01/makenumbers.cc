dd9da6da39e3d48d72a70cd552b75192d7237709
Alexi Stadnitski <alexi.stadnitski@helsinki.fi>
2023-03-03 01:26:33 +0200

    Updating exercise

---
+++ Ex6/Stadnitski/Ex06-01/makenumbers.cc
@@ -0,0 +1,29 @@
+#include "TFile.h"
+#include "TRandom.h"
+#include "TTree.h"
+
+void makenumbers() {
+
+    int N = 1000;
+    TRandom* rng = new TRandom();
+    TFile* normals = TFile::Open("normals.root", "RECREATE");
+    TTree* tree = new TTree("tree", "Tree of normally distributed numbers");
+
+    Float_t n;
+    tree -> Branch("n", &n, "n/F");
+    
+    for (int i = 0; i < N; i++) {
+        n = rng -> Gaus(0, 1);
+        tree -> Fill();
+    }
+    
+    tree -> Write();
+    normals -> Close();
+    exit(0);
+
+}
+
+int main() {
+    makenumbers();
+    return 0;
+}
--- Ex6/Stadnitski/Ex06-01/makeplot.cc
+++ Ex6/Stadnitski/Ex06-01/makeplot.cc
@@ -1,7 +1,46 @@
-#include <iostream>
+#include "TCanvas.h"
+#include "TFile.h"
 #include "TH1F.h"
-using namespace std;
+#include "TTree.h"
+
+void makeplot() {
+
+    TCanvas* canvas = new TCanvas("Canvas", "", 600, 600);
+    canvas -> cd();
+
+    TFile* normals = TFile::Open("normals.root", "READ");
+    TTree* tree = (TTree*) normals -> Get("tree");
+    TH1F* hist = new TH1F("hist", "Standard Gaussian, N = 1000", 100, -4, 4);
+
+    Float_t n;
+    tree -> SetBranchAddress("n", &n);
+    int N = tree -> GetEntries();
+    
+    for (int i = 0; i < N; i++) {
+        tree -> GetEntry(i);
+        hist -> Fill(n);
+    }
+
+    hist -> SetFillColor(5);
+    hist -> SetLineColor(1);
+    hist -> SetLineWidth(3);
+
+    hist -> GetXaxis() -> SetTitle("Simulated variable");
+    hist -> GetXaxis() -> CenterTitle(true);
+    hist -> GetYaxis() -> SetTitle("Number of hits");
+    hist -> GetYaxis() -> CenterTitle(true);
+
+    hist -> Fit("gaus");
+    hist -> SetStats(0);
+    hist -> Draw();
+
+    canvas -> SaveAs("graph.png");
+    canvas -> SaveAs("graph.pdf");
+    canvas -> SaveAs("graph.eps");
+
+}
 
 int main() {
-    cout << "This is .cc file output" << endl;
+    makeplot();
+    return 0;
 }
