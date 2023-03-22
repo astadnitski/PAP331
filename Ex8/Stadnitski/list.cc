77dd4ae6039331f43568f6493524e6d3f7209b40
Alexi Stadnitski <alexi.stadnitski@helsinki.fi>
2023-03-22 20:31:30 +0200

    Updating exercise

---
+++ Ex8/Stadnitski/list.cc
@@ -0,0 +1,7 @@
+#include "Pythia8/Pythia.h"
+using namespace Pythia8;
+
+int main(){
+  Pythia pythia;
+  pythia.particleData.list();
+}
--- Ex8/Stadnitski/width.cc
+++
@@ -1,9 +0,0 @@
-#include "Pythia8/Pythia.h"
-using namespace Pythia8;
-using namespace std;
-
-int main() {
-
-    cout << "Hello." << endl;
-
-}
