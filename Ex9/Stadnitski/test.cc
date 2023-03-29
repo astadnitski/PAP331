d0a2f29e57d7a76b117eb89225b18210e9d57796
Alexi Stadnitski <alexi.stadnitski@helsinki.fi>
2023-03-30 00:29:14 +0300

    Updating exercise

--- Ex9/Stadnitski/test.cc
+++ Ex9/Stadnitski/test.cc
@@ -7,15 +7,31 @@ int main() {
     /// STEP 1: INITIALIZATION ///
 
     Pythia pythia;
-
     pythia.readString("Beams:eCM = 13000.");
+    pythia.readString("PhotonCollision:gmgm2mumu = on");
     pythia.readString("Next:numberShowEvent=0");
     pythia.init();
     
     /// STEP 2: EVENT GENERATION LOOP ///
+
+    Hist transverse("Transverse momentum", 100, 0., 100.);
+    Hist pseudo("Pseudorapidity", 100, 0., 100.);
+    for (int i = 0; i < 1000; i++) {
+        if (!pythia.next()) continue;
+        int select = 0;
+        for (int j = 0; j < pythia.event.size(); j++) if (pythia.event[j].id() == 13) select = j;
+        transverse.fill(pythia.event[select].pT());
+        pseudo.fill(pythia.event[select].phi());
+        cout << i << " : Transverse momentum : " << pythia.event[select].pT() << endl;
+        cout << i << " : Pseudorapidity : " << pythia.event[select].phi() << endl;
+    }
+
     /// STEP 3: PRINT AND SAVE DATA ///
 
-    cout << "Hello" << endl;
+    pythia.stat();
+    cout << transverse;
+    cout << pseudo;
+
     return 0;
 
 }
