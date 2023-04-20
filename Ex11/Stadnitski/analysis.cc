cd0eb636f22073bf0ce91aa4fb4481a09ae59be2
Alexi Stadnitski <alexi.stadnitski@helsinki.fi>
2023-04-20 23:17:52 +0300

    Updating exercise

--- Ex11/Stadnitski/analysis.cc
+++ Ex11/Stadnitski/analysis.cc
@@ -1,8 +1,14 @@
 #include <iostream>
+#include "TFile.h"
 #include "TSelector.h"
+#include "TTree.h"
 using namespace std;
 
 int main() {
+
+    TFile* data = TFile::Open("DYJetsToLL.root", "READ");
+    TTree* tree = (TTree*) data -> Get("Events");
+    tree -> MakeSelector("selector");
 
     cout << "Reached end of main" << endl;
 
